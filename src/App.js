2162301f4fb9bc92aa5e7a97d8b64eb40c3011e8
Axel <axosar@gmail.com>
2019-10-29 10:34:21 -0600

    Prueba de pdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
 import './App.css';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 class App extends Component {
 
@@ -24,24 +26,24 @@ class App extends Component {
           <h1>BIBLIOTECA DIGITAL</h1>
           <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
           <div>
-        <nav>
-          <button onClick={this.goToPrevPage}>Prev</button>
-          <button onClick={this.goToNextPage}>Next</button>
-        </nav>
+            <nav>
+              <button onClick={this.goToPrevPage}>Prev</button>
+              <button onClick={this.goToNextPage}>Next</button>
+            </nav>
 
-        <div style={{ width: 600 }}>
-          <Document
-            file="http://imcyc.com/ArchivosPDF/Fibras%20de%20Acero/4%20CFRP%20Strengthening.pdf"
-            onLoadSuccess={this.onDocumentLoadSuccess}
-          >
-            <Page pageNumber={pageNumber} width={600} />
-          </Document>
-        </div>
+            <div style={{ width: 600 }}>
+              <Document
+                file="http://imcyc.com/ArchivosPDF/Fibras%20de%20Acero/4%20CFRP%20Strengthening.pdf"
+                onLoadSuccess={this.onDocumentLoadSuccess}
+              >
+                <Page pageNumber={pageNumber} width={600} />
+              </Document>
+            </div>
 
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-      </div>
+            <p>
+              Page {pageNumber} of {numPages}
+            </p>
+          </div>
         </header>
       </div>
     );
